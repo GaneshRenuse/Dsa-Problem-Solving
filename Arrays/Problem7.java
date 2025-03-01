98bcbde2eac35220257ede0deb4063cc89946068
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-03-01 22:19:55 +0530

    Created File

---
+++ Arrays/Problem7.java
@@ -0,0 +1,45 @@
+/*
+Problem Statement : 
+You are given a sorted array nums. 
+You need to remove the duplicates in place such that each element appears only once and return the new length. 
+The relative order of the elements should be kept the same.
+Since the array is sorted, the duplicates will be consecutive.
+
+Example:
+Input: nums = [1, 1, 2]
+Output: 2, nums = [1, 2, _]
+
+Input: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+
+Constraints:
+1 <= nums.length <= 10^4
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+
+Test Cases:
+nums = [1, 1, 1, 1, 1] → Output: 1, nums = [1, _, _, _, _]
+nums = [1, 2, 3, 4, 5] → Output: 5, nums = [1, 2, 3, 4, 5]
+nums = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4] → Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+nums = [1] → Output: 1, nums = [1]
+nums = [1, 2, 2, 3, 4, 4, 4, 5, 6, 6] → Output: 6, nums = [1, 2, 3, 4, 5, 6, _, _, _, _]
+*/
+
+import java.util.*;
+
+public class Problem7 {
+    public static int removeDuplicates(int[] nums) {
+        int count = 1;
+        
+        return count;
+    }
+    public static void main(String[] args)
+    {
+        int[] nums = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        int k = removeDuplicates(nums);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
