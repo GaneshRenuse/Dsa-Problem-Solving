1102527b10a4a804855262a95cd6e4d4a6f05260
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-03-02 13:36:23 +0530

    Created Files

---
+++ Arrays/Problem9.java
@@ -0,0 +1,44 @@
+/*
+Problem Statement:
+You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done, and the max profit is 0.
+
+Test Cases:
+prices = [2,4,1] → Output: 2
+prices = [3,2,6,5,0,3] → Output: 4
+prices = [1,2,3,4,5] → Output: 4
+prices = [5,4,3,2,1] → Output: 0
+prices = [7,1,5,3,6,4,8] → Output: 7
+*/
+
+public class Problem9 {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<prices.length; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if(prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices)); // Output: 5
+    }
+}
