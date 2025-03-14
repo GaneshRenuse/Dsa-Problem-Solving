ce065831fa783dfcd8fe4ad7fffb9ffa6ba88c94
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-03-14 23:43:33 +0530

    Updated file

--- Arrays/Problem15.java
+++ Arrays/Problem15.java
@@ -25,7 +25,33 @@ Constraints:
 import java.util.*;
 
 public class Problem15 {
-    public static void main(String[] args) {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersection = new HashSet<>();
 
+        for (int num : nums1) {
+            set.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                intersection.add(num);
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+        int i = 0;
+        for (int num : intersection) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+        int[] result = intersection(nums1, nums2);
+
+        System.out.println(Arrays.toString(result));
     }
 }
