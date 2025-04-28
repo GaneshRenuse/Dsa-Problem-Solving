a73b2b352c19d38cc53af1fc0aebabcc6b036ea5
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-04-28 21:13:19 +0530

    Created new topic directory and ProblemTemplate file

---
+++ Strings/StringsProblem1.java
@@ -0,0 +1,20 @@
+/*
+Problem Statement:
+
+Example 1:
+Input:
+Output:
+Explanation:
+
+Constraints:
+
+Hints/Tips:
+*/
+
+import java.util.*;
+
+class StringsProblem1 {
+    public static void main(String[] args) {
+
+    }
+}
